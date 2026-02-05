1aa974ff999f4eee36cb4eaffeac80510b22f052
Anaid-Ariwany <anaidariwany@gmail.com>
2026-02-05 15:49:11 +0300

    Javascript Assignments: Completed assignment 2 (AddNumbers)

---
+++ JavaScript/Assignments/addNumbers/addNumbers.js
@@ -0,0 +1,16 @@
+function addNumbers() {
+    const a = 1;
+    const b = 1;
+
+    let result;
+
+    result = a + b; // <------ EDITTED THIS LINE
+
+    return result;
+}
+
+//cal the function to see the result
+/* console.log(addNumbers()); */
+
+// Do not change this
+module.exports = addNumbers;
