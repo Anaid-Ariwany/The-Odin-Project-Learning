7f718cb2bc9bab1701a28e1d1e834dc4f5100cb2
Anaid-Ariwany <anaidariwany@gmail.com>
2026-02-05 16:35:27 +0300

    Javascript Assignment: Completed the mathEquations assignment.

---
+++ JavaScript/Assignments/mathEquations/mathEquations.js
@@ -0,0 +1,17 @@
+const a = 1 - 1; // Freebie!!! This is the answer to "one minus one"
+const b = 1 + 8;
+const c = 22 * 3;
+const d = 5 % 4;
+const e = b - 17;
+const f = a + b + c + d + e;
+
+//display the answers in the console
+/* console.log("a:", a);
+console.log("b:", b);
+console.log("c:", c);
+console.log("d:", d);
+console.log("e:", e);
+console.log("f:", f); */
+
+// Do not edit below this line
+module.exports = { a, b, c, d, e, f }
