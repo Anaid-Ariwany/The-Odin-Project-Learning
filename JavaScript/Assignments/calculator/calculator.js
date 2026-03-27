67ae4204529efe390ebf5d23a54179b986760a93
Anaid-Ariwany <anaidariwany@gmail.com>
2026-03-27 15:09:57 +0300

    Calculator assignment completed

---
+++ JavaScript/Assignments/calculator/calculator.js
@@ -0,0 +1,36 @@
+const add = function (a, b) {
+    return a + b;
+};
+
+const subtract = function (a, b) {
+    return a - b;
+};
+
+const sum = function (numbers) {
+    return numbers.reduce((total, num) => total + num, 0);
+};
+
+const multiply = function (numbers) {
+    return numbers.reduce((total, num) => total * num, 1);
+};
+
+const power = function (base, exponent) {
+    return Math.pow(base, exponent);
+};
+
+const factorial = function (n) {
+    if (n === 0) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+};
+
+// Do not edit below this line
+module.exports = {
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
+};
